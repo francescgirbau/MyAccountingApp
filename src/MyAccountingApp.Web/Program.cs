240c0ae5a32dc557c8648c65265a7cfa20c0b250
Francesc Girbau-Llistuella <francescgirbaullistuella@gmail.com>
2026-07-07 22:35:22 +0200

    fix: use HostEnvironment.BaseAddress directly for HttpClient

--- src/MyAccountingApp.Web/Program.cs
+++ src/MyAccountingApp.Web/Program.cs
@@ -9,11 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddMudServices();
 
-string apiBaseUrl = builder.Configuration["ApiBaseUrl"] ?? string.Empty;
-Uri baseAddress = Uri.TryCreate(apiBaseUrl, UriKind.Absolute, out Uri? uri)
-    ? uri
-    : new Uri(builder.HostEnvironment.BaseAddress);
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = baseAddress });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
